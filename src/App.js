f919c18206d8e8ca54820a8313ac16e700f7305e
Giuseppe Milicia <g.milicia@outlook.com>
2022-12-17 11:36:15 +0100

    feat: add base routing with 404 page

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import SideBar from "./components/SideBar";
+import {Outlet, Route, Routes} from "react-router-dom";
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="*" element={<Error404 />} />
+        </Route>
+      </Routes>
+  );
+}
+
+const Layout = () => {
+  return (
+      <div className="container-fluid">
+        <div className="row">
+          <SideBar/>
+          <div className="col-sm p-3 min-vh-100">
+            <Outlet />
+          </div>
+        </div>
+      </div>
   );
 }
 
---
+++ src/pages/Error404.js
@@ -0,0 +1,7 @@
+function Error404() {
+    return (
+        <p>There's nothing here: 404!</p>
+    );
+}
+
+export default Error404;
